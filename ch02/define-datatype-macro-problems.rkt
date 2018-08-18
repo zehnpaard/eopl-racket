93df95b4eda8d639b4e190608e00514d0500fb37
zehnpaard <zehnpaard@googlemail.com>
2018-08-18 20:32:33 +0900

    Problems 2.23 to 2.26

--- ch02/define-datatype-macro-problems.rkt
+++ ch02/define-datatype-macro-problems.rkt
@@ -53,3 +53,108 @@
   (cases stack s
     (empty-stack () #t)
     (non-empty-stack (v r) #f)))
+
+;2.23
+(define (identifier? x)
+  (and (symbol? x) (not (eqv? x 'lambda))))
+(define-datatype lc-exp lc-exp?
+  (var-exp
+   (var identifier?))
+  (lambda-exp
+   (bound-var identifier?)
+   (body lc-exp?))
+  (app-exp
+   (rator lc-exp?)
+   (rand lc-exp?)))
+
+;2.24
+(define-datatype bintree bintree?
+  (leaf-node
+   (num integer?))
+  (interior-node
+   (key symbol?)
+   (left bintree?)
+   (right bintree?)))
+(define (bintree-to-list b)
+  (cases bintree b
+    (leaf-node (num)
+      (list 'leaf-node num))
+    (interior-node (key left right)
+      (list 'interior-node
+            key
+            (bintree-to-list left)
+            (bintree-to-list right)))))
+
+;2.25
+
+(define (max-interior b)
+  (define (node-sum-list-acc node acc)
+    (cases bintree node
+      (leaf-node (n)
+        (cons (list '() n) acc))
+      (interior-node (key left right)
+        (let ((x (node-sum-list-acc left acc)))
+          (let ((y (node-sum-list-acc right x)))
+            (cons (list key (+ (cadr (car x)) (cadr (car y))))
+                  y))))))
+  (define (filter-leafs xs)
+    (cond
+      ((null? xs) xs)
+      ((eqv? '() (car (car xs)))
+       (filter-leafs (cdr xs)))
+      (else
+       (cons (car xs) (filter-leafs (cdr xs))))))
+  (define (node-sum-list node)
+    (filter-leafs (node-sum-list-acc node '())))
+  (define (max-node-acc xs y)
+    (cond
+      ((null? xs)
+       y)
+      ((> (cadr (car xs)) (cadr y))
+       (max-node-acc (cdr xs) (car xs)))
+      (else
+       (max-node-acc (cdr xs) y))))
+  (define (max-node xs)
+    (max-node-acc xs (car xs)))
+  (car (max-node (node-sum-list b))))
+
+;2.26
+(define (all p xs)
+  (if (null? xs)
+    #t
+    (and (p (car xs))
+         (all p (cdr xs)))))
+
+(define (list-of p)
+  (lambda (xs)
+    (and
+     (list? xs)
+     (all p xs))))
+
+(define-datatype red-blue-st red-blue-st?
+  (red-node
+   (left red-blue-st?)
+   (right red-blue-st?))
+  (blue-node
+   (trees (list-of red-blue-st?)))
+  (rbleaf-node
+   (num number?)))
+
+(define (inc x) (+ x 1))
+
+(define (mark-leaves-with-red-depth rbt)
+  (define (mlwrd rbt acc)
+    (cases red-blue-st rbt
+      (red-node (left right)
+        (red-node (mlwrd left (inc acc)) (mlwrd right (inc acc))))
+      (blue-node (trees)
+        (map-mlwrd trees acc))
+      (rbleaf-node (num)
+        (rbleaf-node acc))))
+  (define (map-mlwrd trees acc)
+    (if (null? trees)
+      trees
+      (cons
+       (mlwrd (car trees) acc)
+       (map-mlwrd (cdr trees) acc))))
+  (mlwrd rbt 0))
