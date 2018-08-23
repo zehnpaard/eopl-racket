23b1c32ba0c963610bebf0c18d1d9e8b74f8743c
zehnpaard <zehnpaard@googlemail.com>
2018-08-23 20:13:41 +0900

    Problem 3.21

--- ch03/proc-problems1.rkt
+++ ch03/proc-problems1.rkt
@@ -24,3 +24,34 @@
    (diff-exp (var-exp 'x)
              (diff-exp (const-exp 0)
                        (var-exp 'y)))))
+
+;3.21
+(define-datatype expression expression?
+  (proc-exp
+    (vars (list-of identifier?))
+    (body expression?))
+  (call-exp
+    (rator expression?)
+    (rands (list-of expression?)))
+  )
+
+(define (value-of exp env)
+  (cases expression exp
+    (proc-exp (vars body)
+      (proc-val (procedure vars body env)))
+    (call-exp (rator rands)
+      (apply-procedure
+       (expval->proc (value-of rator env))
+       (map (lambda (rand) (value-of rand env)) rands)))
+    ))
+
+(define-datatype proc proc?
+  (procedure
+   (vars (list-of identifier?))
+   (body expression?)
+   (env environment?)))
+
+(define (apply-procedure proc1 vals)
+  (cases proc proc1
+    (procedure (vars body env)
+      (value-of body (extend-env* vars vals env)))))
