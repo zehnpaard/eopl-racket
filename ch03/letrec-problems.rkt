79fdb133cece5cd6a8e22bd556e84df692996a67
zehnpaard <zehnpaard@googlemail.com>
2018-08-27 07:27:54 +0900

    Problem 3.34

--- ch03/letrec-problems.rkt
+++ ch03/letrec-problems.rkt
@@ -152,3 +152,23 @@
   (cases proc proc1
     (procedure (vars body env)
       (value-of body (extend-env* vars vals env)))))
+
+;3.34
+(define (empty-env)
+  (lambda (var)
+    (eopl:error 'empty-env "Variable ~s not found" var)))
+
+(define (extend-env var val env)
+  (lambda (search-var)
+    (if (eqv? search-var var)
+      val
+      (apply-env env search-var))))
+
+(define (apply-env env var)
+  (env var))
+
+(define (extend-env-rec proc-name arg body)
+  (lambda (search-var)
+    (if (eqv? search-var proc-name)
+      (proc-val (procedure arg body (extend-env-rec proc-name arg body)))
+      (apply-env env search-var))))
