342e41424f0d1f8e89396298ea249c7b0abbfcf0
zehnpaard <zehnpaard@googlemail.com>
2018-09-08 19:25:44 +0900

    Add environment to let-sllgen

--- ch03/let-sllgen.rkt
+++ ch03/let-sllgen.rkt
@@ -1,11 +1,13 @@
 #lang eopl
 
+; Token scanner specification
 (define scanner-spec1
   '((white-sp (whitespace) skip)
     (comment ("%" (arbno (not #\newline))) skip)
     (identifier (letter (arbno (or letter digit))) symbol)
     (number (digit (arbno digit)) number)))
 
+; Grammar specification
 (define let-grammar
   '((program
      (expression)
@@ -29,9 +31,11 @@
      ("let" identifier "=" expression "in" expression)
      let-exp)))
 
+; SLLGEN
 (sllgen:make-define-datatypes scanner-spec1 let-grammar)
 (define scan-parse (sllgen:make-string-parser scanner-spec1 let-grammar))
 
+; expval constructors and accessors
 (define-datatype expval expval?
   (num-val
    (num number?))
@@ -49,3 +53,23 @@
     (cases expval v
       (bool-val (b) b)
       (else (eopl:error 'expval->bool "Cannot convert non-bool-val ~s to boolean" v)))))
+
+; environment constructors and accessors
+
+(define identifier? symbol?)
+
+(define-datatype environment environment?
+  (empty-env)
+  (extend-env
+   (var identifier?)
+   (val expval?)
+   (env environment?)))
+
+(define (apply-env e v)
+  (cases environment e
+    (empty-env ()
+      (eopl:error 'apply-env "Variable ~s not found in environment ~s" v e))
+    (extend-env (v1 val e1)
+      (if (eq? v v1)
+        val
+        (apply-env e1 v)))))
