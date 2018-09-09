52a3ff4e56d7a6ecbb6f09552b8b9924dc9910ac
zehnpaard <zehnpaard@googlemail.com>
2018-09-09 23:03:20 +0900

    Create sllgen grammar for PROC

---
+++ ch03/proc-sllgen.rkt
@@ -0,0 +1,38 @@
+#lang eopl
+
+;Token scanner specification
+(define scanner-spec1
+  '((white-sp (whitespace) skip)
+    (comment ("%" (arbno (not #\newline))) skip)
+    (identifier (letter (arbno (or letter digit))) symbol)
+    (number (digit (arbno digit)) number)))
+
+; Grammar specification
+(define proc-grammar
+  '((program
+     (expression)
+     a-program)
+    (expression
+     (number)
+     const-exp)
+    (expression
+     (identifier)
+     var-exp)
+    (expression
+     ("zero? (" expression ")")
+     zero?-exp)
+    (expression
+     ("- (" expression "," expression ")")
+     diff-exp)
+    (expression
+     ("if" expression "then" expression "else" expression)
+     if-exp)
+    (expression
+     ("let" identifier "=" expression "in" expression)
+     let-exp)
+    (expression
+     ("proc (" identifier ")" expression)
+     proc-exp)
+    (expression
+     ("(" expression "," expression ")")
+     call-exp)))
