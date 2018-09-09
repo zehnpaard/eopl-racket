278be0e389ef00e31d816625af16dfb9a96f4d37
zehnpaard <zehnpaard@googlemail.com>
2018-09-09 23:08:22 +0900

    Add environment and expval

--- ch03/proc-sllgen.rkt
+++ ch03/proc-sllgen.rkt
@@ -41,3 +41,42 @@
 (sllgen:make-define-datatypes scanner-spec1 proc-grammar)
 (define scan-parse (sllgen:make-string-parser scanner-spec1 proc-grammar))
 
+; expval constructors and accessors
+(define-datatype expval expval?
+  (num-val
+   (num number?))
+  (bool-val
+   (bool boolean?)))
+
+(define expval->num
+  (lambda (v)
+    (cases expval v
+      (num-val (n) n)
+      (else (eopl:error 'expval->num "Cannot convert non-num-val ~s to number" v)))))
+
+(define expval->bool
+  (lambda (v)
+    (cases expval v
+      (bool-val (b) b)
+      (else (eopl:error 'expval->bool "Cannot convert non-bool-val ~s to boolean" v)))))
+
+; environment constructors and accessors
+
+(define identifier? symbol?)
+
+(define-datatype environment environment?
+  (empty-env)
+  (extend-env
+   (var identifier?)
+   (val expval?)
+   (env environment?)))
+
+(define (apply-env e v)
+  (cases environment e
+    (empty-env ()
+      (eopl:error 'apply-env "Variable ~s not found in environment ~s" v e))
+    (extend-env (v1 val e1)
+      (if (eq? v v1)
+        val
+        (apply-env e1 v)))))
+
