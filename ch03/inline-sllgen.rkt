9dd84e2bcf5829d4e0340f1db4d82f4a481b6c42
zehnpaard <zehnpaard@googlemail.com>
2018-09-17 19:49:07 +0900

    Add interpreter to INLINE

--- ch03/inline-sllgen.rkt
+++ ch03/inline-sllgen.rkt
@@ -216,3 +216,88 @@
     (else
      (eopl:error 'translation-of "Unable to translate ~s" e))))
 
+; expressions
+(define-datatype expval expval?
+  (num-val
+   (num number?))
+  (bool-val
+   (bool boolean?))
+  (proc-val
+   (proc proc?)))
+
+(define (expval->num v)
+  (cases expval v
+    (num-val (n) n)
+    (else (eopl:error 'expval->num "Cannot convert ~s to number" v))))
+
+(define (expval->bool v)
+  (cases expval v
+    (bool-val (b) b)
+    (else (eopl:error 'expval->bool "Cannot convert ~s to boolean" v))))
+
+(define (expval->proc v)
+  (cases expval v
+    (proc-val (p) p)
+    (else (eopl:error 'expval->proc "Cannot convert ~s to proc" v))))
+
+; nameless environment
+(define (nameless-environment? x)
+  ((list-of expval?) x))
+
+(define (empty-nameless-env)
+  '())
+
+(define (extend-nameless-env val nenv)
+  (cons val nenv))
+
+(define (apply-nameless-env nenv n)
+  (list-ref nenv n))
+
+; procedure
+(define-datatype proc proc?
+  (procedure
+   (body expression?)
+   (penv nameless-environment?)))
+
+(define (apply-procedure proc1 arg)
+  (cases proc proc1
+    (procedure (body penv)
+      (value-of body (extend-nameless-env arg penv)))))
+
+; interpreter
+
+(define (value-of e env1)
+  (cases expression e
+    (const-exp (n)
+      (num-val n))
+    (zero?-exp (exp1)
+      (bool-val (zero? (expval->num (value-of exp1 env1)))))
+    (diff-exp (exp1 exp2)
+      (num-val (- (expval->num (value-of exp1 env1))
+                  (expval->num (value-of exp2 env1)))))
+    (if-exp (cond-exp true-exp false-exp)
+      (if (expval->bool (value-of cond-exp env1))
+        (value-of true-exp env1)
+        (value-of false-exp env1)))
+    (call-exp (func arg)
+      (apply-procedure
+       (expval->proc (value-of func env1))
+       (value-of arg env1)))
+    (nameless-var-exp (n)
+      (apply-nameless-env env1 n))
+    (nameless-let-exp (exp1 body)
+      (value-of body
+        (extend-nameless-env (value-of exp1 env1) env1)))
+    (nameless-proc-exp (body)
+      (proc-val (procedure body env1)))
+    (else
+      (eopl:error 'value-of "Cannot get value of expression ~s" e))))
+
+(define (value-of-program p)
+  (cases program p
+    (a-program (e)
+      (value-of e (empty-nameless-env)))))
+
+(define (run s)
+  (value-of-program
+   (translation-of-program (scan-parse s))))
