c73a75afbfc7c6bf875abc48a3e3e26afbc46072
zehnpaard <zehnpaard@googlemail.com>
2018-09-25 18:47:23 +0900

    Problem 4.8

--- ch04/explicit-ref-problems.rkt
+++ ch04/explicit-ref-problems.rkt
@@ -22,3 +22,25 @@
             (setref! ref1
                      (value-of exp2 env1))
             val1))))))
+
+;4.8
+(define (newref val)
+  (set! the-store (append the-store (list val))) ; append is linear
+  (- (length the-store) 1))
+
+(define (deref ref)
+  (list-ref the-store ref)) ; list-ref is linear
+
+(define (setref! ref val)
+  (define (setref-inner store1 ref1)
+    (cond
+      ((null? store1)
+       (eopl:error 'setref-inner "Invalid reference ~s" ref1))
+      ((zero? ref1)
+       (cons val (cdr store1)))
+      (else
+       (cons
+        (car store1)
+        (setref-inner (cdr store1) (- ref1 1)))))) ; recursive setref-inner is worst-case linear
+  (set! the-store (setref-inner the-store ref)))
+
