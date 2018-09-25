5473e72e5a1460b00920283c3f5d29d7809c2374
zehnpaard <zehnpaard@googlemail.com>
2018-09-25 18:34:06 +0900

    Problem 4.7

--- ch04/explicit-ref-problems.rkt
+++ ch04/explicit-ref-problems.rkt
@@ -9,3 +9,16 @@
           (setref! (expval->ref (value-of exp1 env1))
                    val2)
           val2)))))
+
+;4.7
+(define (value-of e env1)
+  (cases expression e
+    (setref-exp (exp1 exp2)
+      (let ((ref1
+             (expval->ref (value-of exp1 env1))))
+        (let ((val1
+               (deref ref1)))
+          (begin
+            (setref! ref1
+                     (value-of exp2 env1))
+            val1))))))
