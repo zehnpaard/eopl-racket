50091c33be580a3df1e0d7dee5d01fed925a0daa
zehnpaard <zehnpaard@googlemail.com>
2018-10-08 10:28:04 +0900

    Remove unused value-of function and minor refactoring

--- ch05/continuation-passing-interpreter.rkt
+++ ch05/continuation-passing-interpreter.rkt
@@ -3,34 +3,6 @@
     (a-program (exp1)
       (value-of/k exp1 (init-env) (end-cont)))))
 
-(define (value-of exp env)
-  (cases expression exp
-    (const-exp (num) (num-bal num))
-    (var-exp (var) (apply-env env var))
-    (diff-exp (exp1 exp2)
-      (num-val
-        (- (expval->num (value-of exp1 env))
-           (expval->num (value-of exp2 env)))))
-    (zero?-exp (exp1)
-      (bool-val 
-        (zero? (expval->num (value-of exp1 env)))))
-    (if-exp (exp1 exp2 exp3)
-      (if (expval->bool (value-of exp1 env))
-        (value-of exp2 env)
-        (value-of exp3 env)))
-    (let-exp (var exp1 exp2)
-      (value-of body (extend-env var (value-of exp1 env) env)))
-    (proc-exp (var body)
-      (proc-val (procedure var body env)))
-    (call-exp (rator rand)
-      (apply-procedure
-        (expval->proc (value-of rator env))
-        (value-of rand env)))
-    (letrec-exp (p-name b-var p-body letrec-body)
-      (value-of letrec-body
-        (extend-env-rec p-name b-var p-body env)))
-    ))
-
 (define (value-of/k exp env cont)
   (cases expression exp
     (const-exp (num)
@@ -41,9 +13,7 @@
       (apply-cont cont
         (proc-val (procedure var body env))))
     (letrec-exp (p-name b-var p-body letrec-body)
-      (value-of/k letrec-body
-        (extend-env-rec p-name b-var p-body env)
-        cont))
+      (value-of/k letrec-body (extend-env-rec p-name b-var p-body env) cont))
     (zero?-exp (exp1)
       (value-of/k exp1 env (zero1-cont cont)))
     (let-exp (var exp1 body)
