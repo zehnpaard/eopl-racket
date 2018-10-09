5a0e3eae50362235cde031e36aca0d124aff4ed5
zehnpaard <zehnpaard@googlemail.com>
2018-10-09 19:15:37 +0900

    Update apply-procedure to apply-procedure/k

--- ch05/continuation-passing-interpreter.rkt
+++ ch05/continuation-passing-interpreter.rkt
@@ -26,10 +26,10 @@
       (value-of/k rator env (rator-cont rand env cont)))
     ))
 
-(define (apply-procedure proc1 val)
+(define (apply-procedure/k proc1 val cont)
   (cases proc proc1
     (procedure (var body saved-env)
-      (value-of body (extend-env var val saved-env)))))
+      (value-of/k body (extend-env var val saved-env) cont))))
 
 (define (end-cont)
   (lambda (val)
