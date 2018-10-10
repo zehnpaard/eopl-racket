f52c230efb03974869dca869df818396f2c87dfa
zehnpaard <zehnpaard@googlemail.com>
2018-10-10 11:30:16 +0900

    Implement datatype-based apply-cont

--- ch05/trampoline.rkt
+++ ch05/trampoline.rkt
@@ -23,3 +23,20 @@
     (exp3 expression?)
     (env environment?)
     (cont continuation?)))
+
+(define (apply-cont cont val)
+  (cases continuation cont
+    (end-cont ()
+      (begin
+        (eopl:printf "End of computation.~%")
+        val))
+    (zero1-cont (saved-cont)
+      (apply-cont saved-cont
+        (bool-val
+          (zero? (expval->num val)))))
+    (let-exp-cont (var body saved-env saved-cont)
+      (value-of/k body (extend-env var val saved-env) saved-cont))
+    (if-test-cont (exp2 exp3 saved-env saved-cont)
+      (if (expval->bool val)
+        (value-of/k exp2 saved-env saved-cont)
+        (value-of/k exp3 saved-env saved-cont)))))
