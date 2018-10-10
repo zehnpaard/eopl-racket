2f9b354ca48b14e1a4313a4b254415bb7a403738
zehnpaard <zehnpaard@googlemail.com>
2018-10-10 11:27:00 +0900

    Implement datatype representation of continuations

--- ch05/trampoline.rkt
+++ ch05/trampoline.rkt
@@ -8,3 +8,18 @@
   (if (expval? bounce)
     bounce
     (trampoline (bounce))))
+
+(define-datatype continuation continuation?
+  (end-cont)
+  (zero1-cont
+    (cont continuation?))
+  (let-exp-cont
+    (var identifier?)
+    (body expression?)
+    (env environment?)
+    (cont continuation?))
+  (if-test-cont
+    (exp2 expression?)
+    (exp3 expression?)
+    (env environment?)
+    (cont continuation?)))
