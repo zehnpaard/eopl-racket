5c5f87e8ad08104a01cd0ed66081fb2f015a8114
zehnpaard <zehnpaard@googlemail.com>
2018-10-10 11:24:54 +0900

    Implement trampoline function

--- ch05/trampoline.rkt
+++ ch05/trampoline.rkt
@@ -3,3 +3,8 @@
     (a-program (exp)
       (trampoline
         (value-of/k exp (init-env) (end-cont))))))
+
+(define (trampoline bounce)
+  (if (expval? bounce)
+    bounce
+    (trampoline (bounce))))
