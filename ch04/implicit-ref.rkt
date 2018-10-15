fa35a9cdde0584aff3ba69a05a2c6e8d7523d90b
zehnpaard <zehnpaard@googlemail.com>
2018-10-15 10:07:35 +0900

    Add state data and update var-exp

--- ch04/implicit-ref.rkt
+++ ch04/implicit-ref.rkt
@@ -3,7 +3,7 @@
     (const-exp (n)
       (num-val n))
     (var-exp (v)
-      (apply-env env1 v))
+      (deref (apply-env env1 v)))
     (zero?-exp (e)
       (bool-val (zero? (expval->num (value-of e env1)))))
     (diff-exp (e1 e2)
@@ -22,3 +22,40 @@
        (expval->proc (value-of e1 env1))
        (value-of e2 env1)))
     ))
+
+; store
+(define (empty-store)
+  '())
+
+(define the-store 'uninitialized)
+
+(define (get-store)
+  the-store)
+
+(define (initialize-store!)
+  (set! the-store (empty-store)))
+
+(define (reference? v)
+  (integer? v))
+
+(define (newref val)
+  (set! the-store (append the-store (list val)))
+  (- (length the-store) 1))
+
+(define (deref ref)
+  (list-ref the-store ref))
+
+(define (setref! ref val)
+  (define (setref-inner store1 ref1)
+    (cond
+      ((null? store1)
+       (eopl:error 'setref-inner "Invalid reference ~s" ref1))
+      ((zero? ref1)
+       (cons val (cdr store1)))
+      (else
+       (cons
+        (car store1)
+        (setref-inner (cdr store1) (- ref1 1))))))
+  (set! the-store (setref-inner the-store ref)))
+
+
