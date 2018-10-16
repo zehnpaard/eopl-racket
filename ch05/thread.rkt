121123352f084fdc40daf33013270332b873f49d
zehnpaard <zehnpaard@googlemail.com>
2018-10-16 19:53:15 +0900

    Copy continuation datatype definition

--- ch05/thread.rkt
+++ ch05/thread.rkt
@@ -28,6 +28,37 @@
     (procedure (var body saved-env)
       (value-of body (extend-env var (newref val) saved-env)))))
 
+; continuation
+
+(define-datetype continuation continuation?
+  (end-cont)
+  (zero1-cont
+    (saved-cont continuation?))
+  (let-exp-cont
+    (var identifier?)
+    (body expression?)
+    (saved-env environment?)
+    (saved-cont continuation?))
+  (if-test-cont
+    (exp2 expression?)
+    (exp3 expression?)
+    (saved-env environment?)
+    (saved-cont continuation?))
+  (diff1-cont
+    (exp2 expression?)
+    (saved-env environment?)
+    (saved-cont continuation?))
+  (diff2-cont
+    (val1 expval?)
+    (saved-cont continuation?))
+  (rator-cont
+    (rand expression?)
+    (saved-env environment?)
+    (saved-cont continuation?))
+  (rand-cont
+    (val1 expval?)
+    (saved-cont continuation?)))
+
 ; store
 (define (empty-store)
   '())
