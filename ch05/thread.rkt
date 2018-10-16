471723d98825c5a963b6dcd1d26d5eae4c2e871a
zehnpaard <zehnpaard@googlemail.com>
2018-10-16 19:56:58 +0900

    Copy apply-cont function

--- ch05/thread.rkt
+++ ch05/thread.rkt
@@ -23,6 +23,35 @@
       (value-of/k exp1 env (assign1-cont var env cont)))
     ))
 
+
+(define (apply-cont cont val)
+  (cases continuation cont
+    (end-cont ()
+      (begin
+        (eopl:printf "End of computation.~%")
+        val))
+    (zero1-cont (saved-cont)
+      (apply-cont saved-cont
+        (bool-val
+          (zero? (expval->num val)))))
+    (let-exp-cont (var body saved-env saved-cont)
+      (value-of/k body (extend-env var val saved-env) saved-cont))
+    (if-test-cont (exp2 exp3 saved-env saved-cont)
+      (if (expval->bool val)
+        (value-of/k exp2 saved-env saved-cont)
+        (value-of/k exp3 saved-env saved-cont)))
+    (diff1-cont (exp2 saved-env saved-cont)
+      (value-of/k exp2 saved-env (diff2-cont val saved-cont)))
+    (diff2-cont (val1 saved-cont)
+      (apply-cont saved-cont
+        (num-val (- (expval->num val1)
+                    (expval->num val)))))
+    (rator-cont (rand saved-env saved-cont)
+      (value-of/k rand saved-env (rand-cont val saved-cont)))
+    (rand-cont (val1 saved-cont)
+      (apply-procedure/k (expval->proc val1) val saved-cont))
+    ))
+
 (define (apply-procedure proc1 val)
   (cases proc proc1
     (procedure (var body saved-env)
