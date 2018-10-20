32653a599d21755dea5e02f67dc8a2b555efe508
zehnpaard <zehnpaard@googlemail.com>
2018-10-20 20:06:13 +0900

    Modify apply-cont to check time and switch threads

--- ch05/thread.rkt
+++ ch05/thread.rkt
@@ -34,51 +34,58 @@
 
 
 (define (apply-cont cont val)
-  (cases continuation cont
-    (end-cont ()
-      (begin
-        (eopl:printf "End of computation.~%")
-        val))
-    (zero1-cont (saved-cont)
-      (apply-cont saved-cont
-        (bool-val
-          (zero? (expval->num val)))))
-    (let-exp-cont (var body saved-env saved-cont)
-      (value-of/k body (extend-env var val saved-env) saved-cont))
-    (if-test-cont (exp2 exp3 saved-env saved-cont)
-      (if (expval->bool val)
-        (value-of/k exp2 saved-env saved-cont)
-        (value-of/k exp3 saved-env saved-cont)))
-    (diff1-cont (exp2 saved-env saved-cont)
-      (value-of/k exp2 saved-env (diff2-cont val saved-cont)))
-    (diff2-cont (val1 saved-cont)
-      (apply-cont saved-cont
-        (num-val (- (expval->num val1)
-                    (expval->num val)))))
-    (rator-cont (rand saved-env saved-cont)
-      (value-of/k rand saved-env (rand-cont val saved-cont)))
-    (rand-cont (val1 saved-cont)
-      (apply-procedure/k (expval->proc val1) val saved-cont))
-    (assign-cont (var saved-env saved-cont)
-      (begin
-        (setref! (apply-env saved-env var) val)
-        (apply-cont saved-cont (num-val 27))))
-    (spawn-cont (saved-cont)
-      (begin
-        (place-on-ready-queue!
-          (lambda ()
-            (apply-procedure/k
-              (expval->proc val)
-              (num-val 28)
-              (end-subthread-cont))))
-        (apply-cont saved-cont (num-val 73))))
-    (end-main-thread-cont ()
-      (begin
-        (set-final-answer! val)
-        (run-next-thread)))
-    (end-subthread-cont ()
+  (if (time-expired?)
+    (begin
+      (place-on-ready-queue!
+        (lambda () (apply-cont cont val)))
       (run-next-thread))
-    ))
+    (begin
+      (decrement-timer!)
+      (cases continuation cont
+        (end-cont ()
+          (begin
+            (eopl:printf "End of computation.~%")
+            val))
+        (zero1-cont (saved-cont)
+          (apply-cont saved-cont
+            (bool-val
+              (zero? (expval->num val)))))
+        (let-exp-cont (var body saved-env saved-cont)
+          (value-of/k body (extend-env var val saved-env) saved-cont))
+        (if-test-cont (exp2 exp3 saved-env saved-cont)
+          (if (expval->bool val)
+            (value-of/k exp2 saved-env saved-cont)
+            (value-of/k exp3 saved-env saved-cont)))
+        (diff1-cont (exp2 saved-env saved-cont)
+          (value-of/k exp2 saved-env (diff2-cont val saved-cont)))
+        (diff2-cont (val1 saved-cont)
+          (apply-cont saved-cont
+            (num-val (- (expval->num val1)
+                        (expval->num val)))))
+        (rator-cont (rand saved-env saved-cont)
+          (value-of/k rand saved-env (rand-cont val saved-cont)))
+        (rand-cont (val1 saved-cont)
+          (apply-procedure/k (expval->proc val1) val saved-cont))
+        (assign-cont (var saved-env saved-cont)
+          (begin
+            (setref! (apply-env saved-env var) val)
+            (apply-cont saved-cont (num-val 27))))
+        (spawn-cont (saved-cont)
+          (begin
+            (place-on-ready-queue!
+              (lambda ()
+                (apply-procedure/k
+                  (expval->proc val)
+                  (num-val 28)
+                  (end-subthread-cont))))
+            (apply-cont saved-cont (num-val 73))))
+        (end-main-thread-cont ()
+          (begin
+            (set-final-answer! val)
+            (run-next-thread)))
+        (end-subthread-cont ()
+          (run-next-thread))
+  ))))
 
 (define (apply-procedure proc1 val)
   (cases proc proc1
