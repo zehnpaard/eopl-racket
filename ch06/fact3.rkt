122dbf249e62b369cd16a890b6b81e5c41520096
zehnpaard <zehnpaard@googlemail.com>
2018-10-22 18:53:43 +0900

    Implement fact and fact/k

--- ch06/fact3.rkt
+++ ch06/fact3.rkt
@@ -8,3 +8,17 @@
     (n integer?)
     (cont continuation?)))
 
+(define (fact m)
+  (set! cont (end-cont))
+  (set! n m)
+  (fact/k))
+
+(define (fact/k)
+  (if (zero? n)
+    (begin
+      (set! val 1)
+      (apply-cont))
+    (begin
+      (set! cont (fact1-cont n cont))
+      (set! n (- n 1))
+      (fact/k))))
