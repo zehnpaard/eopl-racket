b45b92640b145038fb28c9436ab3b57052722c84
zehnpaard <zehnpaard@googlemail.com>
2018-10-22 18:58:51 +0900

    Implement imperative apply-cont

--- ch06/fact3.rkt
+++ ch06/fact3.rkt
@@ -22,3 +22,12 @@
       (set! cont (fact1-cont n cont))
       (set! n (- n 1))
       (fact/k))))
+
+(define (apply-cont)
+  (cases continuation cont
+    (end-cont ()
+      val)
+    (fact1-cont (saved-n saved-cont)
+      (set! val (* saved-n val))
+      (set! cont saved-cont)
+      (apply-cont))))
