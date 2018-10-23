6afe77dff3c226a23391bab0f16cdc9250c1065f
zehnpaard <zehnpaard@googlemail.com>
2018-10-23 19:46:10 +0900

    Inline end-cont and fact1-cont

--- ch06/fact5.rkt
+++ ch06/fact5.rkt
@@ -1,18 +1,14 @@
 ; CPS factorial function
 (define (fact n)
-  (fact/k n (end-cont)))
+  (fact/k n
+          (lambda (val) val)))
 
 (define (fact/k n cont)
   (if (zero? n)
     (apply-cont cont 1)
-    (fact/k (- n 1) (fact1-cont n cont))))
-
-(define (end-cont)
-  (lambda (val) val))
-
-(define (fact1-cont n saved-cont)
-  (lambda (val) 
-    (apply-cont saved-cont (* n val))))
+    (fact/k (- n 1)
+            (lambda (val)
+              (apply-cont cont (* n val))))))
 
 (define (apply-cont cont val)
   (cont val))
