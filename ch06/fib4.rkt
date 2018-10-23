adef23552ab91be4298a7690dcd47bc6a06c9af2
zehnpaard <zehnpaard@googlemail.com>
2018-10-23 20:10:44 +0900

    Inline continuation-builders

--- ch06/fib4.rkt
+++ ch06/fib4.rkt
@@ -1,21 +1,14 @@
 (define (fib n)
-  (fib/k n (end-cont)))
+  (fib/k n (lambda (val) val)))
 
 (define (fib/k n cont)
   (if (< n 2)
     (apply-cont cont 1)
-    (fib/k (- n 1) (fib1-cont n cont))))
-
-(define (empty-cont)
-  (lambda (val) val))
-
-(define (fib1-cont n saved-cont)
-  (lambda (val)
-    (fib/k (- n 1) (fib2-cont val saved-cont))))
-
-(define (fib2-cont v saved-cont)
-  (lambda (val)
-    (apply-cont saved-cont (+ v val))))
+    (fib/k (- n 1)
+           (lambda (val)
+             (fib/k (- n 1)
+                    (lambda (val)
+                      (apply-cont cont (+ v val))))))))
 
 (define (apply-cont cont val)
   (cont val))
